5e466176e3c2b71fd793cd41217641ee35b9fc81
Ulrich Hecht <uli@kernel.org>
2026-09-11 10:53:06 +0200

    Merge branch 'linux-4.19.y-st' into linux-4.19.y-cip
    Brings the tree up-to-date with 5.10.266.
    
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1733,6 +1733,7 @@ void cpufreq_suspend(void)
 	if (!cpufreq_driver)
 		return;
 
+	cpus_read_lock();
 	if (!has_target() && !cpufreq_driver->suspend)
 		goto suspend;
 
@@ -1752,6 +1753,7 @@ void cpufreq_suspend(void)
 
 suspend:
 	cpufreq_suspended = true;
+	cpus_read_unlock();
 }
 
 /**
